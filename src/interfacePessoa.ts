62979c1dc505fb0ab7e919bf5a53c586540020d7
Fellyph Cintra <Cintra>
2022-11-05 16:45:39 +0000

    adding alias example

--- src/interfacePessoa.ts
+++ src/interfacePessoa.ts
@@ -1,20 +1,27 @@
 interface Pessoa {
     rg: number;
-    nome: string;
+    nome: nomePessoa;
     endereco: Endereco;
     dataNascimento?: Date;
 }
+
+type nomePessoa = string;
 
 interface Endereco {
     rua: string;
     cidade: string;
     numero: string;
-    estado: string;
+    estado?: string;
     apartamento?: string;
 }
 
 let fellyph:Pessoa = {
     rg: 303030,
     nome: "fellyph cintra",
-    endereco: "lagos, portugal"
+    endereco: {
+        estado: "faro",
+        rua: "rua bonita",
+        cidade: "lagos",
+        numero: "31-b"
+    }
 };
