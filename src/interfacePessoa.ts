ea6cecfa1cc2b75b45218566b14ecf17a4e8216a
Fellyph Cintra <Cintra>
2022-11-05 16:31:03 +0000

    adding interface endereco

--- src/interfacePessoa.ts
+++ src/interfacePessoa.ts
@@ -1,8 +1,16 @@
 interface Pessoa {
     rg: number;
     nome: string;
-    endereco: string;
+    endereco: Endereco;
     dataNascimento?: Date;
+}
+
+interface Endereco {
+    rua: string;
+    cidade: string;
+    numero: string;
+    estado: string;
+    apartamento?: string;
 }
 
 let fellyph:Pessoa = {
