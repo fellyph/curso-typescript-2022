9fe5a9165e802d4a7c3a9fee3a35a09985ca6faf
Fellyph Cintra <Cintra>
2022-11-05 16:23:38 +0000

    adding interface pessoa

---
+++ src/interfacePessoa.ts
@@ -0,0 +1,12 @@
+interface Pessoa {
+    rg: number;
+    nome: string;
+    endereco: string;
+    dataNascimento?: Date;
+}
+
+let fellyph:Pessoa = {
+    rg: 303030,
+    nome: "fellyph cintra",
+    endereco: "lagos, portugal"
+};
--- src/teste.ts
+++
@@ -1,5 +0,0 @@
-class Pessoa {
-    rg;
-    nome = "Fellyph";
-    endereco;
-}
