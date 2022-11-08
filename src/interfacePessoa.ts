eb87175ce6bc1357ccd91bba45c16045eb99f45e
Fellyph Cintra <Cintra>
2022-11-08 15:02:09 +0000

    adding generic example

--- src/interfacePessoa.ts
+++ src/interfacePessoa.ts
@@ -22,19 +22,30 @@ enum EstadoCivil {
     Divorciado = "DIVORCIADO"
 }
 
+let enderecoFellyph:Endereco = {
+    estado: "faro",
+    rua: "rua bonita",
+    cidade: "lagos",
+    numero: "31-b"
+}
+
 let fellyph:Pessoa = {
     rg: 303030,
     nome: "fellyph cintra",
     estadoCivil: EstadoCivil.Casado,
-    endereco: {
-        estado: "faro",
-        rua: "rua bonita",
-        cidade: "lagos",
-        numero: "31-b"
-    }
+    endereco: enderecoFellyph
 };
+
 
 function saudacao(pessoa:Pessoa, callback:() => Pessoa ):string {
     let mensagem = `Olá ${pessoa.nome}`;
     return mensagem;
 }
+
+
+function clone<T>(entrada:T):T {
+    return Object.apply({}, entrada);
+}
+
+const fellyphClone:Pessoa = clone(fellyph);
+const copiaEndereco:Endereco = clone(enderecoFellyph);
