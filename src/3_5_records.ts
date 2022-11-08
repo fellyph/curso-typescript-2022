6761c231330601d80d601dee7d9eb575b21b4cd0
Fellyph Cintra <Cintra>
2022-11-08 17:10:49 +0000

    adding new exercise file

---
+++ src/3_5_records.ts
@@ -0,0 +1,9 @@
+let valor:any = {};
+
+valor = "";
+valor = 1230;
+
+let valorFlex:Record<string, string | Date> = {id: "01010"};
+
+valorFlex.nome = "Fellyph";
+valorFlex.dataNascimento = new Date('31-10-2000');
